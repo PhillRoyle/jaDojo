bbc0977b02405e196705de1d2fc2d124b875843c
Phillip Royle <phillip.royle@bbc.co.uk>
2018-11-08 08:04:17 +0000

    Phill - modifies pangram solution to include some other people's from exercism

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,7 +1,7 @@
 module.exports = {
-    /*trying our map, filter, reduce - build array from phrase, map over to make it lowercase, 
+    /*trying out map, filter, reduce - build array from phrase, map over to make it lowercase, 
     filter to remove non-alpha characters, then reduce to add each char to a set.*/
-    isPangram: (phrase) => {
+    my_isPangram: (phrase) => {
         let filteredSet = phrase.split(``)
             .map((char) => {
                 return char.toLowerCase();
@@ -13,5 +13,16 @@ module.exports = {
                 return acc.add(char);
             }, new Set());
         return [...filteredSet].length === 26;
+    },
+    /* genius! adapted from malydok's solution in exercism */
+    malydok_isPangram: (phrase) => {
+        const text = phrase.toLowerCase().match(/[a-z]/g);
+        return new Set(text).size === 26;
+    },
+    /* also genius! adapted from angamaiton's solution in exercism */
+    isPangram: (phrase) => {
+        const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+        const text = phrase.toLowerCase();
+        return alphabet.every(letter => text.includes(letter));
     }
 }
